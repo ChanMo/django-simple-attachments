e442c963f0837c42845a298717919c035d5ed2d5
dyou <chen.orange@aliyun.com>
2020-08-05 10:54:20 +0800

    use  setup.cfg

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="django-simple-attachments",
-    version="0.1.3",
-    author="ChanMo",
-    author_email="chan.mo@outlook.com",
-    description="Django Simple Attachment Model",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ChanMo/django-simple-attachments",
-    license="MIT",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    keywords="django attachment image storage postgres"
-)
+setup()
