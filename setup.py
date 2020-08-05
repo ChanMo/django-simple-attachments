f9e44f156ef843308da457100f8e929e301e50e6
dyou <chen.orange@aliyun.com>
2020-08-05 10:40:15 +0800

    add keyword

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-simple-attachments",
-    version="0.1.2",
+    version="0.1.3",
     author="ChanMo",
     author_email="chan.mo@outlook.com",
     description="Django Simple Attachment Model",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ChanMo/django-simple-attachments",
+    license="MIT",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -19,4 +20,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    keywords="django attachment image storage postgres"
 )
